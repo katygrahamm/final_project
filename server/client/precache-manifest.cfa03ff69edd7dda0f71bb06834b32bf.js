ed788437d8e3c94da1af17924a4907db82f87a80
katygrahamm <58748932+katygrahamm@users.noreply.github.com>
2020-05-14 15:26:51 -0400

    Add files via upload

---
+++ server/client/precache-manifest.cfa03ff69edd7dda0f71bb06834b32bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b301a2e9ee62f725de3ce4d0417a1a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e443a6463d9d95b57b7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5f17cd26753bb4a2b4f0",
+    "url": "/static/css/main.f19572a3.chunk.css"
+  },
+  {
+    "revision": "4e443a6463d9d95b57b7",
+    "url": "/static/js/2.f88bfb42.chunk.js"
+  },
+  {
+    "revision": "b9859787c9ddcd5cc013f14ae4ad4ac7",
+    "url": "/static/js/2.f88bfb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f17cd26753bb4a2b4f0",
+    "url": "/static/js/main.d1e12994.chunk.js"
+  },
+  {
+    "revision": "79488aec6acb85f75836",
+    "url": "/static/js/runtime-main.5787576a.js"
+  }
+]);

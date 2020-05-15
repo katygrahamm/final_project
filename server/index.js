67a62d09eeca99b5ea878bd1f53a5203fd558dfb
Katy Andersen <katherineandersen@sandiego.edu>
2020-05-15 15:14:25 -0400

    change index.html path

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 

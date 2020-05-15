0d340cbdb9e57779aae28bac2a6a4f40570a2c61
Katy Andersen <katherineandersen@sandiego.edu>
2020-05-15 15:23:53 -0400

    fix server issues

--- server/index.js
+++ server/index.js
@@ -16,19 +16,6 @@ const cookieSession = require('cookie-session')
 // DB Setup
 mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true });
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.use(
     cors({
@@ -311,6 +298,19 @@ app.post('/addwishlist', (req, res) => {
   })
 }) 
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Server Setup
 const port = process.env.PORT || 5000;
